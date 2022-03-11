0846d3b611e492e9dfed66d1c5a812959ee8c374
ashleymays091 <ashleymays091@gmail.com>
2022-03-10 19:12:42 -0800

    Added undirected graph with adjacency matrix representation

--- Data Structures/Graphs/Undirected Graphs/Adjacency-Matrix.cpp
+++ Data Structures/Graphs/Undirected Graphs/Adjacency-Matrix.cpp
@@ -1,13 +1,11 @@
 /*
-	Undirected Graph - Adjacency List
+	Undirected Graph - Adjacency Matrix
 
-	An undirected graph represented as an adjacency list. We use the
-	index values 0 to size - 1 to represent the nodes in the graph.
+	An undirected graph represented as an adjacency matrix. We use the
+	index values 0 to size - 1 to represent the keys in the graph.
 	In a graph, we have a set of vertices and a set of edges.
 	An edge connects two vertices. In an undirected graph, the
-	order of the two vertices doesn't matter. In this implementation,
-	we use a vector array instead of a list array for easier access and
-	deletion.
+	order of the two vertices doesn't matter.
 
 	Read more at https://www.geeksforgeeks.org/graph-and-its-representations/
 */
@@ -16,14 +14,14 @@
 #include <iostream>
 #include <vector>
 
-void addEdge(std::vector<int> adj[4], int u, int v)
+void addEdge(std::vector<std::vector<int>>& adj, int u, int v)
 {
 	// since this is an undirected graph, we add a connection to and from both vertices 'u' and 'v'
-	adj[u].push_back(v);
-	adj[v].push_back(u);
+	adj[u][v] = 1;
+	adj[v][u] = 1;
 }
 
-void display(const std::vector<int> adj[4])
+void display(const std::vector<std::vector<int>>& adj)
 {
 	for (int i = 0; i < 4; ++i)
 	{
@@ -38,7 +36,11 @@ void display(const std::vector<int> adj[4])
 
 int main()
 {
-	std::vector<int> adj[4]; // 4 vertices in this graph
+	// Create the adjacency matrix; graph has 4 vertices
+	// We initialize the adjacency matrix as a 4 x 4 square matrix
+	// filled with 0's. When we add an edge between two nodes, the
+	// value will be changed to 1.
+	std::vector<std::vector<int>> adj(4, std::vector<int>(4, 0));
 
 	addEdge(adj, 0, 1);
 	addEdge(adj, 0, 2);
